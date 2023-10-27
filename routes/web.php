07ba784dda3d42714f28d827fbb07695179e5221
aunglynn550 <aunglynn550@gmail.com>
2023-10-27 22:45:30 +0630

    Frontend Template Set Up

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/home', [FrontendController::class, 'index'])->name('home');
 
 Route::get('/', function () {
     return view('welcome');
