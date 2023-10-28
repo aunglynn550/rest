c1cf10b17b562d28ff5cdb13336d7f881c73b984
aunglynn550 <aunglynn550@gmail.com>
2023-10-28 11:22:40 +0630

    Setting Up Login Template | Toastr

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="{{ asset('frontend/css/venobox.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/jquery.exzoom.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('frontend/css/toastr.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
@@ -107,10 +107,24 @@
     <script src="{{ asset('frontend/js/wow.min.js') }}"></script>
     <!-- ex zoom js -->
     <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
+    <!-- Toastr js -->
+    <script src="{{ asset('frontend/js/toastr.min.js') }}"></script>
 
     <!--main/custom js-->
     <script src="{{ asset('frontend/js/main.js') }}"></script>
 
+    <script>    
+                toastr.options.progressBar = true;
+                //toastr.options.closeButton = true;
+                // toastr.options.showMethod = 'slideDown';
+                // toastr.options.hideMethod = 'slideUp';
+                // toastr.options.closeMethod = 'slideUp';
+        @if($errors->any())
+            @foreach($errors->all() as $error)
+                toastr.error("{{  $error }}")
+            @endforeach
+        @endif
+    </script>
 </body>
 
 </html>
