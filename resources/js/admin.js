0f3382ac790a8d281a5c53848e1a68cb30ca9ec2
aunglynn550 <aunglynn550@gmail.com>
2023-12-01 11:21:48 +0630

    Sending real time message User To Admin

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,4 +1,18 @@
+function scrollToBottom(){
+    let chatContent = $('.chat-content');
+    chatContent.scrollTop(chatContent.prop("scrollHeight"));
+}
+
 window.Echo.private('chat.'+loggedInUserId)
 .listen('ChatEvent', (e)=>{
-      console.log(e);
+     
+    let  html = `
+      <div class="chat-item chat-left" style=""><img src="${e.avatar}">
+          <div class="chat-details">
+              <div class="chat-text">${e.message}</div>
+              <div class="chat-time">Sending...</div>
+          </div>
+      </div>`
+  $('.chat-content').append(html)
+  scrollToBottom();
     });
